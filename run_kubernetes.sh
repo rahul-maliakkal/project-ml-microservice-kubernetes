73e2116ba8e2c8fc9d515b5e6bb0fb7c5a7fda8e
Rahul Maliakkal <rahulmaliakkal@Rahuls-MacBook-Pro.local>
2021-02-15 17:21:38 +0530

    Add circle ci config

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=docker.io/rahul14m93/udacity_test:0.1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
+kubectl create deployment django-deployment --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/django-deployment 8000:80
+
+kubectl logs `kubectl get pods -o=name`
+
 
